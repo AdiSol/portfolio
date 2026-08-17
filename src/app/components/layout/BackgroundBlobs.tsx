084ba00a1257a667c07df3ca4623bc8c48ac975a
Adrienne <adriennesoliven@gmail.com>
2026-08-17 22:02:10 +0800

    style: css themes and background

---
+++ src/app/components/layout/BackgroundBlobs.tsx
@@ -0,0 +1,73 @@
+export function BackgroundBlobs() {
+    return (
+        <div
+            aria-hidden="true"
+            className="absolute inset-0 -z-10 overflow-hidden"
+        >
+            <svg
+                viewBox="0 0  1373 2296"
+                preserveAspectRatio="xMidYMid slice"
+                className="h-full w-full"
+            >
+                <g clipPath="url(#clip0_106_8)">
+                    <g filter="url(#filter0_f_106_8)">
+                    <path d="M603.953 878.163C603.953 937.592 842.709 1082.4 705.62 1082.4C547.163 1141.02 -402.263 1362.44 -402.263 1303.01C-402.263 1243.58 -304.416 291.914 -291.153 65.5173C-154.064 65.5173 840.138 836.553 603.953 878.163Z" fill="var(--color-blob-1)"/>
+                    <path d="M603.953 878.163C603.953 937.592 842.709 1082.4 705.62 1082.4C547.163 1141.02 -402.263 1362.44 -402.263 1303.01C-402.263 1243.58 -304.416 291.914 -291.153 65.5173C-154.064 65.5173 840.138 836.553 603.953 878.163Z" fill="url(#pattern0_106_8)"/>
+                    </g>
+
+                    <g filter="url(#filter1_f_106_8)">
+                    <path d="M1448.19 980.007C2038.85 1695.82 767.395 1036.12 534.944 966.377C302.493 896.631 231.131 818.249 291.659 616.524C352.186 414.799 -397.211 -235.536 -164.76 -165.79C1252.82 -55.0928 1936.42 999.007 1448.19 980.007Z" fill="var(--color-blob-2)"/>
+                    <path d="M1448.19 980.007C2038.85 1695.82 767.395 1036.12 534.944 966.377C302.493 896.631 231.131 818.249 291.659 616.524C352.186 414.799 -397.211 -235.536 -164.76 -165.79C1252.82 -55.0928 1936.42 999.007 1448.19 980.007Z" fill="url(#pattern1_106_8)"/>
+                    </g>
+
+                    <g filter="url(#filter2_f_106_8)">
+                    <path d="M918.906 482.63C1384.42 130.198 1706.35 1334.4 865.035 617.91C821.301 795.216 739.706 762.005 806.065 552.178C125.2 1227.42 309.063 142.352 404.526 93.3405C499.988 44.3287 1782.35 80.3872 918.906 482.63Z" fill="var(--color-blob-3)"/>
+                    <path d="M918.906 482.63C1384.42 130.198 1706.35 1334.4 865.035 617.91C821.301 795.216 739.706 762.005 806.065 552.178C125.2 1227.42 309.063 142.352 404.526 93.3405C499.988 44.3287 1782.35 80.3872 918.906 482.63Z" fill="url(#pattern2_106_8)"/>
+                    </g>
+                    
+                    <g filter="url(#filter3_f_106_8)">
+                    <path d="M1514.02 33.0467C1561.01 88.6128 1652.45 567.037 1592.38 617.838C1420.26 1408.08 1593.94 450.764 1172.28 293.107C1125.29 237.541 810.86 45.278 845.143 -27.4186C905.217 -78.2187 1467.03 -22.5193 1514.02 33.0467Z" fill="var(--color-blob-4)"/>
+                    <path d="M1514.02 33.0467C1561.01 88.6128 1652.45 567.037 1592.38 617.838C1420.26 1408.08 1593.94 450.764 1172.28 293.107C1125.29 237.541 810.86 45.278 845.143 -27.4186C905.217 -78.2187 1467.03 -22.5193 1514.02 33.0467Z" fill="url(#pattern3_106_8)"/>
+                    </g>
+                </g>
+                <defs>
+                <filter id="filter0_f_106_8" x="-783.263" y="-315.483" width="1912.34" height="2009.53" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                <feGaussianBlur stdDeviation="190.5" result="effect1_foregroundBlur_106_8"/>
+                </filter>
+                <pattern id="pattern0_106_8" patternContentUnits="objectBoundingBox" width="0.890172" height="0.820822">
+                <use xlinkHref="#image0_106_8" transform="scale(0.000869308 0.000801584)"/>
+                </pattern>
+                <filter id="filter1_f_106_8" x="-590.628" y="-551.998" width="2576.59" height="2243.42" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                <feGaussianBlur stdDeviation="190.5" result="effect1_foregroundBlur_106_8"/>
+                </filter>
+                <pattern id="pattern1_106_8" patternContentUnits="objectBoundingBox" width="0.477549" height="1.00525">
+                <use xlinkHref="#image0_106_8" transform="scale(0.000466356 0.000981694)"/>
+                </pattern>
+                <filter id="filter2_f_106_8" x="-72.9294" y="-304.683" width="1840.99" height="1535.66" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                <feGaussianBlur stdDeviation="190.5" result="effect1_foregroundBlur_106_8"/>
+                </filter>
+                <pattern id="pattern2_106_8" patternContentUnits="objectBoundingBox" width="1.03465" height="0.941359">
+                <use xlinkHref="#image0_106_8" transform="scale(0.0010104 0.000919296)"/>
+                </pattern>
+                <filter id="filter3_f_106_8" x="461.537" y="-428.385" width="1531.63" height="1736.01" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                <feGaussianBlur stdDeviation="190.5" result="effect1_foregroundBlur_106_8"/>
+                </filter>
+                <pattern id="pattern3_106_8" patternContentUnits="objectBoundingBox" width="1.75478" height="0.884762">
+                <use xlinkHref="#image0_106_8" transform="scale(0.00171366 0.000864026)"/>
+                </pattern>
+                <clipPath id="clip0_106_8">
+                <rect width="1373" height="2296" fill="white"/>
+                </clipPath>
+                </defs>
+            </svg>
+        </div>
+    )
+}
