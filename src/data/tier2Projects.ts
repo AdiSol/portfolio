3c554875744bbd4927e82880476a97a0681450e0
Adrienne <adriennesoliven@gmail.com>
2026-08-27 16:10:47 +0800

    data: added demo vid link for akakdsph project

--- src/data/tier2Projects.ts
+++ src/data/tier2Projects.ts
@@ -8,7 +8,8 @@ export const tier2Projects: ProjectCardData[] = [
     role: "Lead Frontend Developer",
     stack: ["React.js", "MaterialUI", "Usability Testing"],
     coverImage: "/images/akadsph-cover.jpg",
-    repoUrl: "https://github.com/joshmzndev/akadsph"
+    repoUrl: "https://github.com/joshmzndev/akadsph",
+    liveUrl: "https://youtu.be/Cz8kp0ibN4Q?t=87"
   },
   {
     title: "NU-LPR 101",
